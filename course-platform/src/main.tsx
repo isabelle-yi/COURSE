8ab861e8757839860b1e43bd1ee256eb494a8a08
易修然 <yixiuran@163.com>
2026-03-26 20:06:32 +0800

    调整匹配react与typescript的版本

--- course-platform/src/main.tsx
+++ course-platform/src/main.tsx
@@ -3,18 +3,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider } from 'antd' // 1. 引入配置组件
+import zhCN from 'antd/locale/zh_CN' // 2. 引入中文包
 import App from './App'
 import './index.css'
 
-// 创建 React Query 客户端
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    {/* 3. 包裹全局，设置中文和主题 */}
+    <ConfigProvider locale={zhCN}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
